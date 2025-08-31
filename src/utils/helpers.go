52ecacccce37b5302d1ea961069fb2ef1383c71b
RickyFirmansyah27 <ricky_firmansyah@teknokrat.ac.id>
2025-08-31 13:58:04 +0700

    remvoe save base 64

--- src/utils/helpers.go
+++ src/utils/helpers.go
@@ -17,20 +17,3 @@ func EncodeImageToBase64(imagePath string) (string, error) {
 	encoded := base64.StdEncoding.EncodeToString(imageData)
 	return encoded, nil
 }
-
-// SaveBase64ToImage decodes base64 image data and saves it to a file
-func SaveBase64ToImage(base64Data, outputPath string) error {
-	// Decode base64 data
-	decoded, err := base64.StdEncoding.DecodeString(base64Data)
-	if err != nil {
-		return err
-	}
-
-	// Write to file
-	err = ioutil.WriteFile(outputPath, decoded, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
